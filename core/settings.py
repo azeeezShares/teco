3ec880089ef99d3603792e599241104441edad99
Azizbek <azizbekqozoqovinfo@email.com>
2025-04-07 14:24:38 +0500

    some settings change

--- core/settings.py
+++ core/settings.py
@@ -155,3 +155,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'account.CustomUser'
+LOGIN_URL = "admin_login"
