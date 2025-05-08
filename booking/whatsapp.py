496796885818df0f1065758e2e6cbc21f08f8c22
Azeeez <azizbekqozoqovinfo@gmail.com>
2025-05-08 15:44:14 +0500

    Some whatsapp changes

--- booking/whatsapp.py
+++ booking/whatsapp.py
@@ -1,5 +1,5 @@
 import requests
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 
@@ -7,6 +7,8 @@ import json
 ACCESS_TOKEN ="EAATOF1eYUM8BO9EQGpJVKt00c6MMmFZBPN7puy3fD0DKLZAeAzvOnEPBcksGF6fwmvgEPgH5NdGw7Tahhp6zE1Lr3ZBo9PK9Rtdvo9NTXUjwoFnbbu80YRO8c14YBmCS4hAka0O9emI5USrwfx6jzO7lHNWqbrvfZC1O1QS710iqepOnliNoPHWG"
 VERSION = "v22.0"
 PHONE_NUMBER_ID = "682771568245963"
+VERIFY_TOKEN = "dsalfjdfjoieoijfdlskjdjfdslkfjdkjfd"  # Replace with your actual token
+WEBHOOK_URL = "https://tecoshop.es/webhook/"  # Replace with your actual webhook URL
 
 def send_message(data):
     headers = {
@@ -58,5 +60,13 @@ def webhook(request):
             return JsonResponse({"status": "success"}, status=200)
         except json.JSONDecodeError:
             return JsonResponse({"error": "Invalid JSON"}, status=400)
-    else:
-        return JsonResponse({"error": "Only POST method allowed"}, status=405)
+    
+    elif request.method == 'GET':
+        verify_token = request.GET.get('hub.verify_token')
+        challenge = request.GET.get('hub.challenge')
+        mode = request.GET.get('hub.mode')
+
+        if mode and verify_token == VERIFY_TOKEN:
+            return HttpResponse(challenge)
+        else:
+            return HttpResponse("Invalid verification token", status=403)
